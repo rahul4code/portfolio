bfdf7264aa47d625b34a561e59ea884a5e8500b4
rahul4code <you@example.com>
2021-09-08 17:16:10 +0530

    Updates

--- sw.js
+++ sw.js
@@ -27,7 +27,7 @@ workbox.core.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-06140a99247f22caa05a.js"
+    "url": "webpack-runtime-0d89120cfda16602301f.js"
   },
   {
     "url": "framework-eb684e3e828ad13b3940.js"
@@ -37,7 +37,7 @@ self.__precacheManifest = [
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "295d47a3784314bc22be1d2173c55953"
+    "revision": "ac44114a481b0051cb54f54ad0435a46"
   },
   {
     "url": "component---cache-caches-gatsby-plugin-offline-app-shell-js-a95b272f8c25b9a8a758.js"
@@ -48,7 +48,7 @@ self.__precacheManifest = [
   },
   {
     "url": "page-data/app-data.json",
-    "revision": "3a573dcf62a5d3b2a2d6176baa836d2e"
+    "revision": "17c8c246b3c8c8cc5ff26603a6c79b54"
   },
   {
     "url": "polyfill-b600bf296a322334b4a5.js"
